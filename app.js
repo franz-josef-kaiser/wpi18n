9086a4a123b4a78dce386709a6fa8f1827bee9cc
Franz Josef Kaiser <wecodemore@gmail.com>
2016-08-15 16:38:39 +0200

    Refactor: Clean up obsolte routes and comments

--- app.js
+++ app.js
@@ -5,56 +5,13 @@ var Hapi    = require( 'hapi' ),
     Promise = require( 'bluebird' ),
     Joi     = require( 'joi' ),
     Zip     = require( 'jszip' );
-var fs = require('fs');
+
 var server   = new Hapi.Server(),
     handlers = {};
 
 server.connection( {
 	host : '0.0.0.0',
 	port : process.env.PORT || 5000
-} );
-
-/*server.route( {
-	method : 'GET',
-	path   : '/plugin/packages.json',
-	config : {
-		handler  : function( request, reply ) {
-
-			var writeStream = fs.createWriteStream( 'packages.json' );
-			var stream = fs.createReadStream( writeStream );
-			stream.pipe( {
-				packages : {
-					"foo/bar" : {
-					}
-				}
-			} );
-			// @TODO bluebird
-			new Promise.resolve( stream ).then( function( json ) {
-				console.info( json );
-				reply( json )
-					.header( 'Content-Type', 'application/json' );
-			} );
-		}
-	}
-} );*/
-
-server.route( {
-	method : 'GET',
-	path   : '/{scope}/downloads',
-	config : {
-		handler  : function( request, reply ) {
-console.info( request.payload );
-			reply( {
-				"downloads" : [
-					{
-						name    : "test/name",
-						version : "1.0.0"
-					}
-				]
-			} )
-				.type( 'application/json' );
-		}
-	}
 } );
 
 server.route( {
@@ -83,12 +40,6 @@ console.info( request.payload );
 					'attachment; filename=packages.json'
 				)
 				.type( 'application/json' );
-
-			/*reply( {
-				params : request.params,
-				query  : request.query
-			} )
-				.header( 'Content-Type', 'application/json' );*/
 		},
 		validate : {
 			params : {
