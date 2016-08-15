8e01900c3b70e09a6106d769982c3459206f12ab
Franz Josef Kaiser <wecodemore@gmail.com>
2016-08-15 16:27:43 +0200

    Feat: Add packages.json to endpoint to have something to answer

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var Hapi    = require( 'hapi' ),
     Promise = require( 'bluebird' ),
     Joi     = require( 'joi' ),
     Zip     = require( 'jszip' );
-
+var fs = require('fs');
 var server   = new Hapi.Server(),
     handlers = {};
 
@@ -14,18 +14,81 @@ server.connection( {
 	port : process.env.PORT || 5000
 } );
 
-server.route( {
+/*server.route( {
 	method : 'GET',
-	path   : '/{scope?}',
+	path   : '/plugin/packages.json',
 	config : {
 		handler  : function( request, reply ) {
+
+			var writeStream = fs.createWriteStream( 'packages.json' );
+			var stream = fs.createReadStream( writeStream );
+			stream.pipe( {
+				packages : {
+					"foo/bar" : {
+					}
+				}
+			} );
+			// @TODO bluebird
+			new Promise.resolve( stream ).then( function( json ) {
+				console.info( json );
+				reply( json )
+					.header( 'Content-Type', 'application/json' );
+			} );
+		}
+	}
+} );*/
+
+server.route( {
+	method : 'GET',
+	path   : '/{scope}/downloads',
+	config : {
+		handler  : function( request, reply ) {
+console.info( request.payload );
 			reply( {
+				"downloads" : [
+					{
+						name    : "test/name",
+						version : "1.0.0"
+					}
+				]
+			} )
+				.type( 'application/json' );
+		}
+	}
+} );
+
+server.route( {
+	method : 'GET',
+	path   : '/{scope}/packages.json',
+	config : {
+		handler  : function( request, reply ) {
+console.info( request.payload );
+
+			reply( {
+				packages : {
+					"lang/plugin" : {
+						name    : "test/name",
+						version : "1.0.0",
+						dist    : {
+							url  : request.connection.info.protocol
+								+ '://' + request.info.host + request.url.path,
+							type : "zip"
+						}
+					},
+					"lang/theme" : {}
+				}
+			} )
+				.header(
+					'Content-disposition',
+					'attachment; filename=packages.json'
+				)
+				.type( 'application/json' );
+
+			/*reply( {
 				params : request.params,
 				query  : request.query
 			} )
-				.header( 'Content-Type', 'application/json' );
-			/*reply( { foo : 'bar' } )
-			 .header( 'Content-Type', 'application/json' );*/
+				.header( 'Content-Type', 'application/json' );*/
 		},
 		validate : {
 			params : {
